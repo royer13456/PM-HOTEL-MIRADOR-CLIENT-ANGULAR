a2691930caa6ccf521a9b42067da7b1bf79787c6
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-03-15 21:20:47 -0500

    añadiedo rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { RoomComponent } from './pages/room/room.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutComponent } from './pages/about/about.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminGuard } from './pages/admin/admin.guard';
+import { LoginAdminComponent } from './pages/login-admin/login-admin.component';
 
 const routes: Routes = [
   {
@@ -17,6 +20,12 @@ const routes: Routes = [
   },
   {
     path: 'contacto', component: ContactUsComponent
+  },
+  {
+    path: 'admin', component: AdminComponent, canActivate: [AdminGuard]
+  },
+  {
+    path: 'login', component: LoginAdminComponent
   },
 ];
 
