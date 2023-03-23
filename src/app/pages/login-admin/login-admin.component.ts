048a3bd2b5f27a995670623f2a799bb8a395e30a
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-03-23 14:16:43 -0500

    FIX: login fixed

--- src/app/pages/login-admin/login-admin.component.ts
+++ src/app/pages/login-admin/login-admin.component.ts
@@ -21,14 +21,15 @@ export class LoginAdminComponent implements OnInit {
 
   login() {
     const { user, password } = this.admin;
-    const token = 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db';
-
     this.adminService.isLoggedIn(user, password)
       .subscribe(response => {
-        this.LSService.set('admin', token);
-        if (response) {
-          this.adminService.isLogged = true; // Actualiza el estado de inicio de sesión
+
+        if (response === true) {
+          const token = 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db';
+          this.LSService.set('admin', token);
+          this.adminService.isLogged = true;
           this.router.navigate(['/admin']);
+          window.history.replaceState({}, '', '/admin');
         } else {
           console.error("Error");
         }
