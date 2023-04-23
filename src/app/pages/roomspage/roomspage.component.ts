d24b5128ab97cf331a418e6bf419afd393f0cfc9
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-04-22 20:38:23 -0500

    feat: improve code in roomspage

--- src/app/pages/roomspage/roomspage.component.ts
+++ src/app/pages/roomspage/roomspage.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { Room } from 'src/app/interface';
 import { RoomService } from 'src/app/services/room.service';
+
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
 import { RoomcardComponent } from 'src/app/components/client/roomcard/roomcard.component';
@@ -10,34 +12,41 @@ import { CardskeletonComponent } from 'src/app/shared/cardskeleton/cardskeleton.
 @Component({
   selector: 'app-roomspage',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent, RoomcardComponent, CardskeletonComponent],
   templateUrl: './roomspage.component.html',
-  styleUrls: ['./roomspage.component.css']
+  styleUrls: ['./roomspage.component.css'],
+  imports: [
+    CommonModule,
+    NavbarComponent,
+    FooterComponent,
+    RoomcardComponent,
+    CardskeletonComponent
+  ]
 })
 export class RoomspageComponent implements OnInit {
-
   public roomList: Room[] = [];
-
-  public descriptions: string[] = []
-
+  public descriptions: string[] = [];
   public estandarRooms: Room[] = [];
-
   public dobleRooms: Room[] = [];
-
   public tripleRooms: Room[] = [];
 
-  private roomService = inject(RoomService)
+  private roomService = inject(RoomService);
 
   constructor() { }
 
   ngOnInit() {
-    this.roomService.getRoomsRequest()
-      .subscribe(res => {
+    this.roomService
+      .getRoomsRequest()
+      .subscribe((res) => {
         this.roomList = res;
-        this.estandarRooms = this.roomList.filter(room => room.category === "Estándar");
-        this.dobleRooms = this.roomList.filter(room => room.category === "Doble ejecutivo");
-        this.tripleRooms = this.roomList.filter(room => room.category === "Tripe ejecutivo");
-      })
+        this.estandarRooms = this.roomList.filter(
+          (room) => room.category === 'Estándar'
+        );
+        this.dobleRooms = this.roomList.filter(
+          (room) => room.category === 'Doble ejecutivo'
+        );
+        this.tripleRooms = this.roomList.filter(
+          (room) => room.category === 'Tripe ejecutivo'
+        );
+      });
   }
-
 }
